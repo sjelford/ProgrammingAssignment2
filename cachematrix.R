c0f6d9cdd8db55e419a01ba15bd514723cc71789
sjelford <30474894+sjelford@users.noreply.github.com>
2017-08-17 21:41:11 +0200

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,36 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The makeCacheMatrix creates a special "matrix" by assigning values 
+## to a different environment
 
-## Write a short comment describing this function
+## makeCacheMatirx creates a special matrix "x"
 
 makeCacheMatrix <- function(x = matrix()) {
+		m <- NULL
+        set <- function(y){
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setmatrix <- function(solve) m <<- solve
+        getmatrix <- function() m
+        list(set = set, get = get,
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse matrix of "x" 
+## but it checks to see if values already cached
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getmatrix()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setmatrix(m)
+        m
 }
